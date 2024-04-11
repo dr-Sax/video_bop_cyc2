e863ee5595d65d2908aa3441b5e34046456fe3b8
dr-Sax <nicoromano2000@gmail.com>
2024-04-11 10:44:34 -0400

    blow auto input opts

--- projects/active_imagination.py
+++ projects/active_imagination.py
@@ -3,32 +3,10 @@
 ##################################################
 import dash_bootstrap_components as dbc
 from dash import Dash, html, dcc, Input, Output
-import pandas as pd
 
 import json
-haiku_list = [] 
-txt_area_list = []
 
-with open('assets/bnh_transcript.json') as json_data:
-    d = json.loads(json_data.read())
 
-    segments = d['segments']
-    for seg in segments:
-        haiku_list.append(seg['text'])
-        ta = dcc.Textarea(id= f'{seg["text"]}',value = '', style={'width': '100%', 'height': 50})
-        txt_area_list.append(ta)
-
-    json_data.close()
-
-rule_fmt = []
-for i in range(0, len(haiku_list)):
-    tmp = [
-    html.Hr(),
-    html.H4(haiku_list[i]),
-    txt_area_list[i]
-    ]
-    
-    rule_fmt += tmp
 
 ##################################################
 # Constants
@@ -42,13 +20,6 @@ p1 = "Active imagination refers to a process or technique of engaging with the i
 
 WIDTH = 1100
 HEIGHT = 1000
-size = 2
-
-youtube = html.Embed(
-        src = "https://www.youtube.com",
-        width = WIDTH,
-        height = HEIGHT
-)
 
 header =  dbc.Row(
             children = [
@@ -56,8 +27,7 @@ header =  dbc.Row(
                     [
                         html.Hr(),
                         html.H4(p1),
-                        html.Hr(),
-                        rule_fmt
+                        html.Hr()
                     ]
                 )
                 
@@ -65,6 +35,7 @@ header =  dbc.Row(
 
             style = {"width":"10", "textAlign":"left"}
         ) 
+
 
 ##################################################
 # Mapping Page Content
--- projects/blow.py
+++ projects/blow.py
@@ -3,21 +3,47 @@
 ##################################################
 import dash_bootstrap_components as dbc
 from dash import Dash, html, dcc, Input, Output
-import pandas as pd
+
+import json
+haiku_list = [] 
+txt_area_list = []
+haiku_slc_opts = []
+
+note_rows = []
+
+for i in range(1, 9):
+    with open(f'assets/h{i}_transcript.json') as json_data:
+        d = json.loads(json_data.read())
+        segments = d['segments']
+
+        for seg in segments:
+            haiku_list.append(seg['text'])
+
+        haiku_slc_opts.append({'label': f'{haiku_list[i - 1]}', 'value': i - 1})
+        json_data.close()
+
+haiku_select = dbc.Select(
+    id="haiku-select",
+    required=True,
+    placeholder = "Which Haiku you Blowing Over?",
+    options=haiku_slc_opts
+)
 
 ##################################################
 # Constants
 ##################################################
-    
+p1 = "The Jazz term for 'improvise.' It has a more mystical aura. Also, simply to play an instrument."
+
+
+youtube = dcc.Link(children = 'Youtube', href = 'www.youtube.com')
+
 header =  dbc.Row(
             children = [
                 html.Div(
                     [
                         html.Hr(),
-                        html.H4("The Jazz term for 'improvise.' It has a more mystical aura. Also, simply to play an instrument."),
-                        html.Hr(),
-                        dbc.Col(
-                        )
+                        html.H4(p1),
+                        html.Hr()
                     ]
                 )
                 
@@ -26,13 +52,21 @@ header =  dbc.Row(
             style = {"width":"10", "textAlign":"left"}
         ) 
 
+input_rows =  html.Div(
+                id = 'input-rows',
+                children = []
+                )
+
 ##################################################
 # Mapping Page Content
 ##################################################     
 def page_content():
         body = dbc.Container(
             [
-                header
+                header,
+                haiku_select,
+                youtube,
+                input_rows
             ]
         )
         return body
@@ -40,13 +74,65 @@ def page_content():
 # ##################################################
 # # Page Specific Callbacks
 # ##################################################     
-# def get_callbacks(app):
-#     @app.callback(
-#         Output(component_id = 'index_tables', component_property = 'children'),
-#         Input(component_id = 'index_select', component_property = 'value')
-#     )
-#     def gen_hilbert_table(order_n):
-#         df = pd.read_csv(f'assets/hilbert/hilbert_index_table_{order_n}.csv')
-#         table = dbc.Table.from_dataframe(df, striped=True, bordered=True, hover=True)
-#         return table
+def get_callbacks(app):
+    @app.callback(
+        Output(component_id = 'input-rows', component_property = 'children'),
+        Input(component_id = 'haiku-select', component_property = 'value')
+    )
+    def gen_blow_inpts(haiku):
+        if haiku is not None:
+            phrase = haiku_list[int(haiku)]
+            word_list = phrase.strip().split()
+
+            input_rows_list = []
+            word_opts_list = []
+            for i in range(0, len(word_list)):
+                 word_opts_list.append({'label': f'{word_list[i]}', 'value': f'{word_list[i]}'})
+
+            for i in range(0, len(word_list)):
+                 
+                input = dbc.Row(
+                            children = [
+                                dbc.Col(
+                                     dbc.Input(
+                                          id = f"url-{i}",
+                                          required=False,
+                                          placeholder="Paste URL"
+                                     )
+                                     
+                                ),
+                                dbc.Col(
+                                     dbc.Input(
+                                          id = f"strt-time-{i}",
+                                          required=False,
+                                          placeholder="Paste Clip Start-time"
+                                     )
+                                     
+                                ),
+                                dbc.Col(
+                                     dbc.Select(
+                                        id=f"start-word-{i}",
+                                        required=False,
+                                        placeholder = "Start Word",
+                                        options=word_opts_list
+                                    )
+                                ),
+
+                                dbc.Col(
+                                     dbc.Select(
+                                        id="end-word-{i}",
+                                        required=False,
+                                        placeholder = "End Word",
+                                        options=word_opts_list
+                                    )
+                                )
+
+                        
+                            ],
+
+                            style = {"width":"10", "textAlign":"left"}
+                        ) 
+                input_rows_list.append(input)
+            return input_rows_list
+
 
